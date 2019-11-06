c7708c23c550189792c8abd11f68f9ff82c5b448
sylpro <sylpro42@gmail.com>
2019-11-06 17:35:27 +0900

    packet

--- src/Packet.java
+++ src/Packet.java
@@ -1,16 +1,21 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Packet {
+@WebServlet("/Packet")
+public class Packet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	private ArrayList<Item> items = new ArrayList<Item>();
-    
+       
     public Packet() {
         super();
     }
-
-	public void addItem(Item item) {
+    public void addItem(Item item) {
 		this.items.add(item);
 	}
 	public String raw() {
@@ -20,10 +25,23 @@ public class Packet {
 		}
 		return result.toString();
 	}
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Packet packet = new Packet();
 		packet.addItem(Item.create("key", 6, Format.time()));
 		packet.addItem(Item.create("text", 1000, request.getParameter("Txt")));
+		
+		try {
+			Write.writeFile(packet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("/Project/");
 	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
 }
--- src/Write.java
+++ src/Write.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Write {
 	public static void writeFile(Packet packet) throws Exception {
-		Path path = Paths.get("D://2019", Format.date() + ".txt");
+		Path path = Paths.get("/opt/tomcat/2019", Format.date() + ".txt");
 		ArrayList<String> contents = new ArrayList<String>();
 		contents.add(packet.raw());
 		Charset charset = Charset.forName("EUC-KR");
