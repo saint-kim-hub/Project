15ca716c07d1eeb380691dc01ad8a8f10ffef5cc
sylpro <sylpro42@gmail.com>
2019-11-11 09:10:04 +0900

    덮어쓰기 문제 해결

--- src/Write.java
+++ src/Write.java
@@ -3,12 +3,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Write {
 	public static void writeFile(Packet packet) throws Exception {
-		Path path = Paths.get("/opt/tomcat/2019", Format.format() + ".txt"); // 패킷을 저장할 경로 지정. /opt/tomcat/년도/날짜.txt
-//		Path path = Paths.get("d://2019", Format.format() + ".txt");
+		Date d = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmmss");
+		
+		Path path = Paths.get("/opt/tomcat/2019", sdf.format(d) + ".txt"); // 패킷을 저장할 경로 지정. /opt/tomcat/년도/날짜.txt
+//		Path path = Paths.get("d://2019", sdf.format(d) + ".txt");
 		ArrayList<String> contents = new ArrayList<String>(); // packet 내용을 저장할 배열 리스트
 		contents.add(packet.raw()); // packet내용 저장
 		Charset charset = Charset.forName("UTF-8"); // 인코딩 방식 
